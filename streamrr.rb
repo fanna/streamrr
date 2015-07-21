598d88dfe9512e4e5b482413a76ef84ba5d16aea
fanna <fanna993@gmail.com>
2015-07-21 17:12:53 +0200

    Fixing indentation

--- streamrr.rb
+++ streamrr.rb
@@ -6,7 +6,7 @@ require_relative "lib/pusher_sender.rb"
 set :port, 8989
 
 post "/" do
-	posted_string = params[:q]
+  posted_string = params[:q]
   puts posted_string
   PusherSender.to_pusher(posted_string)
 end
